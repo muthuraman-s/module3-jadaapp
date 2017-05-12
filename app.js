34b50d3303df7b76b08277c3a3d776bacfa0e971
Muthuraman sethuramalingam <muthuraman-s@users.noreply.github.com>
2017-05-12 17:07:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/',routes.home);
 app.get('/:hotel',routes.hotel);
 
-var port = process.env.PORT || 8082;
+var port = process.env.PORT || 8088;
 
 var server=app.listen(port,function(req,res){
     console.log("Catch the action at http://localhost:"+port);
